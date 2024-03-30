fe881449cd6454c80d10f44eece3409ae2f2685a
hassan0184 <mianhassanshahzad@gmail.com>
2024-03-30 15:35:08 +0500

    added camera functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from location.models import Location
+from location.models import Location,Booth
 
 def Dashboard_View(request):
-    locations = Location.objects.all()  
-    return render(request, 'home.html', {'locations': locations})
+    locations = Location.objects.all()
+    booths = Booth.objects.all()  
+    return render(request, 'home.html', {'locations': locations,'booths': booths})
--- location/views.py
+++ location/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
-from .models import Location,Booth
+from .models import Location,Booth,Camera
 from django.contrib import messages
 
 @staff_member_required
@@ -23,9 +23,9 @@ def location_management(request):
 def booth_management(request): 
     if request.method == 'POST':
         booth_name = request.POST.get('booth_name')
-        booth_location_id = request.POST.get('booth_location')  # Get location id from the form
+        booth_location_id = request.POST.get('booth_location')  
         if booth_name and booth_location_id:
-            booth_location = Location.objects.get(pk=booth_location_id)  # Get the Location object using the id
+            booth_location = Location.objects.get(pk=booth_location_id) 
             Booth.objects.create(name=booth_name, location=booth_location)
             messages.success(request, 'Booth Added Successfully')
             return redirect('home')
@@ -36,4 +36,20 @@ def booth_management(request):
 @staff_member_required
 @login_required
 def camera_management(request):
-    pass
+    if request.method == 'POST':
+        camera_name = request.POST.get('camera_name')
+        location_id = request.POST.get('location') 
+        booth_id = request.POST.get('booth')
+        
+        if camera_name and location_id and booth_id:
+            location = Location.objects.get(pk=location_id)
+            booth = Booth.objects.get(pk=booth_id)
+            
+            Camera.objects.create(name=camera_name, location=location, booth=booth)
+            
+            messages.success(request, 'Camera Added Successfully')
+            return redirect('home')
+    
+    cameras = Camera.objects.all()
+    return render(request, 'camera_management.html', {'cameras': cameras})
+
