69fc3ba121f0d69e7348802863fc28e08270b36a
rafay-waqar-99 <raffaywaqar792@gmail.com>
2024-04-19 13:09:48 +0500

    intehrated html into ip camers

---
+++ static/js/camera_stream.js
@@ -0,0 +1,6 @@
+const video = document.getElementById('camera-stream');
+
+video.src = '/location/view-camera-feed/';
+
+video.play();
+// we will handle the capture image and uplaod it to the bucket functionality here 
