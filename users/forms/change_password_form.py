9c5d31b7888a07075aa15cd8b507206c82e93413
rafay-waqar-99 <raffaywaqar792@gmail.com>
2024-04-30 12:07:53 +0500

    Added password validation

--- users/forms/change_password_form.py
+++ users/forms/change_password_form.py
@@ -9,6 +9,15 @@ class ChangePasswordForm(forms.Form):
     confirm_password=forms.CharField()
 
     def clean(self):
+        if len(self.cleaned_data.get("password")) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long")
+        has_uppercase = any(char.isupper() for char in self.cleaned_data.get("password"))
+        has_lowercase = any(char.islower() for char in self.cleaned_data.get("password"))
+        has_numeric = any(char.isdigit() for char in self.cleaned_data.get("password"))
+        if not (has_uppercase and has_lowercase and has_numeric):
+        
+            raise forms.ValidationError("Password must contain both uppercase and lowercase characters and and one numeric digit")
+
         if self.cleaned_data.get("new_password") != self.cleaned_data.get("confirm_password"):
             raise forms.ValidationError("new password and confirm password must match")
         return self.cleaned_data
